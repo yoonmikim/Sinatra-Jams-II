10645eb6eae83f7d70676f4620aead008528896c
Eric Kim <hyeokjungkim@gmail.com>
2019-04-03 18:27:09 -0400

    artists controller

---
+++ app/controllers/ArtistsController.rb
@@ -0,0 +1,39 @@
+class ArtistsController < ApplicationController
+  get "/artists" do
+    @artists = Artist.all
+    erb :'artists/index'
+  end
+
+  get "/artists/new" do
+    @artist = Artist.new
+    erb :'artists/new'
+  end
+
+  get "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    erb :'artists/show'
+  end
+
+  get "/artists/:id/edit" do
+    @artist = Artist.find(params[:id])
+    erb :'artists/edit'
+  end
+
+  patch "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    @artist.update(params[:artist])
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  post "/artists" do
+    @artist = Artist.create(params[:artist])
+    redirect to "/artists"
+  end
+
+  delete "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect to "/artists"
+  end
+
+end
