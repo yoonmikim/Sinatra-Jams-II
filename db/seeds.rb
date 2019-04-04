b55ed8dbc6b7e7935c1454187a8246eb1ba020fa
Eric Kim <hyeokjungkim@gmail.com>
2019-04-04 11:46:53 -0400

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,8 @@ Artist.destroy_all
   Artist.create(name: Faker::Name.name, age: rand(1..100), bio: Faker::Hipster.sentence)
 end
 
+5.times do
+  Instrument.create(name: Faker::Music.instrument, brand: Faker::Company.name)
+end
+
 puts "File has been seeded! 🍀"
